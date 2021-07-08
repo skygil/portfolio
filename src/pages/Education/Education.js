dc72f13e40c9eaec5ccec6fb1b0e6412a1d176cf
Skye Gilbreth <skyigilb@gmail.com>
2021-07-08 13:11:38 -0700

    created barrel pattern for education

--- src/pages/Education/Education.js
+++ src/pages/Education/Education.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Layout from '../../components/Layout';
+import {React, Layout} from './index';
 // eslint-disable-next-line
 import { SectionTitle, Paragraph, ListItem, Emphasis, Heading1, Heading2, HoverLink, BulletList } from '../../styles';
 
--- src/pages/Education/index.js
+++ src/pages/Education/index.js
@@ -1,2 +1,6 @@
 import Education from './Education'
 export default Education
+
+/* Create a Barrel Pattern to grab all utilized components */
+export { default as React} from 'react';
+export { default as Layout} from  '../../components/Layout';
