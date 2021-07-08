5954feb121909ad9fc6061e5657a341bcd08f857
Skye Gilbreth <skyigilb@gmail.com>
2021-07-08 13:27:38 -0700

    added Barrel Pattern to userHeader

--- src/components/UserHeader/index.js
+++ src/components/UserHeader/index.js
@@ -1,2 +1,7 @@
 import UserHeader from './UserHeader'
 export default UserHeader
+
+/* Create a Barrel Pattern to grab all utilized components */
+export { default as React} from 'react';
+// export { default as useLocation} from  'react-router-dom';
+// export { default as ArrowRight16} from  '@carbon/icons-react';
