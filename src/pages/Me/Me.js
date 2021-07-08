92b6cc90e6e14273c76baac72f5860226b841072
Skye Gilbreth <skyigilb@gmail.com>
2021-07-08 13:14:22 -0700

    Added barrel pattern to Me section

--- src/pages/Me/Me.js
+++ src/pages/Me/Me.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Layout from '../../components/Layout';
+import {React, Layout} from './index';
 import { SectionTitle, Paragraph, Pill, ProfileLink} from '../../styles';
 
 const Me = ({ user }) => {
--- src/pages/Me/index.js
+++ src/pages/Me/index.js
@@ -1,2 +1,6 @@
 import Me from './Me'
 export default Me
+
+/* Create a Barrel Pattern to grab all utilized components */
+export { default as React} from 'react';
+export { default as Layout} from  '../../components/Layout';
